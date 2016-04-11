b8e401e0377e4cdaa437618096451537eb262b87
charlesmarvin <marvin.charles@gmail.com>
2016-04-10 22:51:07 -0400

    Refactor mongo connection uri

--- src/main/java/io/mmc/Service.java
+++ src/main/java/io/mmc/Service.java
@@ -1,6 +1,8 @@
 package io.mmc;
 
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
@@ -18,7 +20,7 @@ public class Service extends Application<ServiceConfiguration> {
 
     public static void main(String[] args) throws Exception {
         if (args == null || args.length == 0) {
-            String configFile = String.format("./config/%s.yml", System.getenv("env").toLowerCase());
+            String configFile = String.format("./config/%s.yml", System.getenv("ENV").toLowerCase());
             args = new String[] { "server", ClassLoader.getSystemResource(configFile).getFile() };
         }
         new Service().run(args);
@@ -37,6 +39,12 @@ public class Service extends Application<ServiceConfiguration> {
                 return configuration.swaggerBundleConfiguration;
             }
         });
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
     }
 
     @Override
--- src/main/java/io/mmc/ServiceConfiguration.java
+++ src/main/java/io/mmc/ServiceConfiguration.java
@@ -1,10 +1,11 @@
 package io.mmc;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import io.dropwizard.Configuration;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.support.MongoRepositoryFactory;
 
 import java.net.UnknownHostException;
@@ -15,11 +16,11 @@ import java.net.UnknownHostException;
 public class ServiceConfiguration extends Configuration {
     @JsonProperty("swagger")
     public SwaggerBundleConfiguration swaggerBundleConfiguration;
-    @JsonProperty("mongo")
-    public MongoConfiguration mongoConfiguration;
+    @JsonProperty("mongoConnectionUri")
+    public String mongoConnectionUri;
 
     public MongoRepositoryFactory getMongoRepositoryFactory() throws UnknownHostException {
-        MongoTemplate template = new MongoTemplate(new MongoClient(mongoConfiguration.host, mongoConfiguration.port), mongoConfiguration.database);
+        MongoTemplate template = new MongoTemplate(new SimpleMongoDbFactory(new MongoClientURI(mongoConnectionUri)));
         return new MongoRepositoryFactory(template);
     }
 }
